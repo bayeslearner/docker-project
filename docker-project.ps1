0625e5aa3afdfd3c70dca7c7404823b948c5cf70
bayeslearner <imkundeng@gmail.com>
2023-02-15 00:32:13 -0500

    Update docker-project.ps1

--- docker-project.ps1
+++ docker-project.ps1
@@ -1,76 +1,109 @@
-[CmdletBinding()]
-Param(
-    [Parameter(Mandatory=$false, Position=0)]
-    [string]$configdirectory = $pwd,
+function Show-Help {
+    $helpText = @"
+NAME
+    docker-project.ps1 - Script to run docker-compose commands on specified directories.
 
-    [Parameter(Mandatory=$false)]
-    [string]$configname = "docker-project.yaml",
+SYNOPSIS
+    docker-project.ps1 [-Project <String>] [-ConfigDirectory <String>] [-ConfigFile <String>] [-Command <String>] [<CommonParameters>]
 
-    [Parameter(Mandatory=$false)]
-    [string]$project = "default",
+DESCRIPTION
+    This script reads a YAML configuration file and runs specified docker-compose commands on the directories listed in the file.
 
-    [Parameter(Mandatory=$true, Position=1)]
-    [string]$command
-)
+    Required Parameters:
+        -Command <String>
+            The docker-compose command to run. Options are: up, down, logs.
+        -Project <String>
+            The name of the project as specified in the configuration file.
 
-function Invoke-CommandInDirectory {
+    Optional Parameters:
+        -ConfigDirectory <String>
+            The path to the directory where the configuration file is located. Default is the current directory.
+        -ConfigFile <String>
+            The name of the configuration file. Default is 'docker-project.yaml'.
+
+EXAMPLES
+    docker-project.ps1 -Command up -Project default
+    docker-project.ps1 -Command logs -Project project1 -ConfigDirectory c:\config\ -ConfigFile my-config.yaml
+
+"@
+    Write-Host $helpText
+}
+
+function Run-Command {
     param(
-        [Parameter(Mandatory=$true)]
-        [string]$directory,
-        
-        [Parameter(Mandatory=$true)]
-        [string]$command
+        [string]$command,
+        [string]$directory
     )
 
-    Write-Host "Running command '$command' in directory '$directory'"
-    Set-Location -Path $directory
-    Invoke-Expression $command
-    Set-Location -Path $script:initialDirectory
-}
-
-try {
-    $initialDirectory = $pwd
-    $configPath = Join-Path -Path $configdirectory -ChildPath $configname
-
-    if (-not(Test-Path $configPath)) {
-        throw "Config file '$configPath' not found"
-    }
-
-    $config = Get-Content -Path $configPath | ConvertFrom-Yaml
-    $projects = $config.psobject.Properties | Where-Object { $_.Type.Name -eq "Array" } | Select-Object -ExpandProperty Name
-
-    if ($project -ne "all" -and -not($projects.Contains($project))) {
-        throw "Project '$project' not found in config file"
-    }
-
-    if ($project -eq "all") {
-        $projectsToProcess = $projects
+    if ($command -eq "up") {
+        $command = "docker-compose up -d"
+    } elseif ($command -eq "logs") {
+        Write-Host "Command logs not implemented yet"
+        return
+    } elseif ($command.StartsWith("make")) {
+        $command = $command.Substring(4)
     } else {
-        $projectsToProcess = $project
+        $command = "docker-compose $command"
     }
 
-    foreach ($projectToProcess in $projectsToProcess) {
-        $directories = $config.$projectToProcess
-        $commandToExecute = "docker-compose $($command)"
+    Set-Location $directory
+    Write-Host "Running command '$command' on directory '$directory'"
+    Invoke-Expression $command
+}
 
-        if ($command -eq "up") {
-            $commandToExecute += " -d"
-        }
+function Invoke-Project {
+    param(
+        [string]$projectName,
+        [string]$command,
+        [string]$configDirectory,
+        [string]$configFile
+    )
 
-        foreach ($directory in $directories) {
-            if ($command -eq "logs") {
-                Write-Host "The logs command is not implemented yet"
-            } elseif ($command -like "make*") {
-                Invoke-CommandInDirectory -directory $directory -command $command
-            } else {
-                Invoke-CommandInDirectory -directory $directory -command $commandToExecute
-            }
-        }
+    Write-Host "Invoking project '$projectName' with command '$command'"
+
+    $config = Get-YamlConfig -ConfigDirectory $configDirectory -ConfigFile $configFile
+
+    $project = $config[$projectName]
+
+    if (-not $project) {
+        Write-Host "Project '$projectName' not found in configuration file"
+        return
+    }
+
+    foreach ($directory in $project) {
+        Run-Command -command $command -directory $directory
     }
 }
-catch {
-    Write-Host "Error: $_" -ForegroundColor Red
+
+function Get-YamlConfig {
+    param(
+        [string]$configDirectory = ".",
+        [string]$configFile = "docker-project.yaml"
+    )
+
+    $configPath = Join-Path $configDirectory $configFile
+
+    if (-not (Test-Path $configPath)) {
+        Write-Host "Configuration file '$configPath' not found"
+        exit
+    }
+
+    $config = Get-Content $configPath | Out-String | ConvertFrom-Yaml
+    return $config
 }
-finally {
-    Set-Location -Path $initialDirectory
-}
+
+param (
+    [Parameter(Mandatory=$false)]
+    [string]$Project = "default",
+
+    [Parameter(Mandatory=$true)]
+    [string]$Command,
+
+    [Parameter(Mandatory=$false)]
+    [string]$ConfigDirectory = ".",
+
+    [Parameter(Mandatory=$false)]
+    [string]$ConfigFile = "docker-project.yaml"
+)
+
+Invoke-Project -projectName $Project -command $Command -configDirectory $ConfigDirectory -configFile $ConfigFile
