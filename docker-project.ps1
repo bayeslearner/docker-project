a06e0a8f4734b118303e209979193152d1b8ca7a
bayeslearner <bayeslearner@gmail.com>
2023-02-21 18:11:20 -0500

    modify chatGPT auto generated code

--- docker-project.ps1
+++ docker-project.ps1
@@ -1,30 +1,102 @@
-function Show-Help {
-    $helpText = @"
-NAME
+<#
+.NAME
     docker-project.ps1 - Script to run docker-compose commands on specified directories.
 
-SYNOPSIS
+.SYNOPSIS
     docker-project.ps1 [-Project <String>] [-ConfigDirectory <String>] [-ConfigFile <String>] [-Command <String>] [<CommonParameters>]
 
-DESCRIPTION
+.DESCRIPTION
     This script reads a YAML configuration file and runs specified docker-compose commands on the directories listed in the file.
 
     Required Parameters:
         -Command <String>
-            The docker-compose command to run. Options are: up, down, logs.
-        -Project <String>
-            The name of the project as specified in the configuration file.
+            The docker-compose command to run. 
+
+
 
     Optional Parameters:
         -ConfigDirectory <String>
             The path to the directory where the configuration file is located. Default is the current directory.
         -ConfigFile <String>
             The name of the configuration file. Default is 'docker-project.yaml'.
+        -Project <String>
+            The name of the project as specified in the configuration file.
 
-EXAMPLES
-    docker-project.ps1 -Command up -Project default
-    docker-project.ps1 -Command logs -Project project1 -ConfigDirectory c:\config\ -ConfigFile my-config.yaml
+.EXAMPLES
+    docker-project.ps1 -Project default -Command up 
+    docker-project.ps1 -Project default -Command up 
+    docker-project.ps1 -Project project1  -f ./compose-budibase/docker-compose.yaml config
+    docker-project.ps1 -Project project1 -ConfigDirectory c:\config\ -ConfigFile my-config.yml -f ./compose-budibase/docker-compose.yaml config
 
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [string]$Project = "default",
+
+    [Parameter(Position=0, ValueFromRemainingArguments= $true)]
+    [string]$Command="up",
+
+    [Parameter(Mandatory=$false)]
+    [string]$ConfigDirectory = ".",
+
+    [Parameter(Mandatory=$false)]
+    [string]$ConfigFile = "docker-project.yml",
+
+    [Parameter(ParameterSetName="Help")]
+    [switch]$Help
+)
+
+
+function Show-Help {
+    $helpText = @"
+.NAME
+    docker-project.ps1 - Script to run docker-compose commands on specified directories.
+
+.SYNOPSIS
+    docker-project.ps1 [-Project <String>] [-ConfigDirectory <String>] [-ConfigFile <String>] [-Command <String>] [<CommonParameters>]
+
+.DESCRIPTION
+    This script reads a YAML configuration file and runs specified docker-compose commands on the directories listed in the file.
+
+    Required Parameters:
+        -Command <String>
+            The docker-compose command to run. 
+
+
+
+    Optional Parameters:
+        -ConfigDirectory <String>
+            The path to the directory where the configuration file is located. Default is the current directory.
+        -ConfigFile <String>
+            The name of the configuration file. Default is 'docker-project.yaml'.
+        -Project <String>
+            The name of the project as specified in the configuration file.
+
+.EXAMPLES
+    docker-project.ps1 -Project default -Command up 
+    docker-project.ps1 -Project default -Command up 
+    docker-project.ps1 -Project project1  -f ./compose-budibase/docker-compose.yaml config
+    docker-project.ps1 -Project project1 -ConfigDirectory c:\config\ -ConfigFile my-config.yaml -f ./compose-budibase/docker-compose.yaml config
+
+.CONFIG
+default: &default
+  - /path/to/project1
+  - /path/to/project2
+  - /path/to/project3
+
+project1: &project1
+  - /path/to/project4
+  - /path/to/project5
+
+project2: &project2
+  - /path/to/project6
+
+project3:
+  - /path/to/project7
+  - *project1
+  - *project2
 "@
     Write-Host $helpText
 }
@@ -41,14 +113,53 @@ function Run-Command {
         Write-Host "Command logs not implemented yet"
         return
     } elseif ($command.StartsWith("make")) {
-        $command = $command.Substring(4)
+        $command = $command
     } else {
         $command = "docker-compose $command"
     }
-
+    Push-Location
     Set-Location $directory
     Write-Host "Running command '$command' on directory '$directory'"
-    Invoke-Expression $command
+    Pop-Location
+    #Invoke-Expression $command
+}
+
+function Get-AbsolutePath {
+    param(
+        [string]$Path,
+        [string]$BaseDirectory
+    )
+
+    if (Test-Path $Path) {
+        return @((Convert-Path $Path), $true)
+    } else {
+        $absolutePath = Join-Path $BaseDirectory $Path
+        if (Test-Path $absolutePath) {
+            return @((Convert-Path $absolutePath), $true)
+        } else {
+            return @($null, $false)
+        }
+    }
+}
+
+function RecursiveUnroll {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory, ValueFromPipeline)]
+        [object[]] $Unroll
+    )
+
+    
+    process {
+        foreach($item in $Unroll) {
+            if($item -is [object[]] -or $item -is [System.Collections.Generic.List`1[System.Object]]) {
+                RecursiveUnroll -Unroll $item
+                continue
+            }
+            #write-host $item.GetType()
+            $item
+        }
+    }
 }
 
 function Invoke-Project {
@@ -70,9 +181,15 @@ function Invoke-Project {
         return
     }
 
-    foreach ($directory in $project) {
-        if (Test-Path $directory) {
-            Run-Command -command $command -directory $directory
+    $projectUnrolled = ((RecursiveUnroll -Unroll $project)) | Select-Object -Unique
+
+    Write-Host "total projects:$($projectUnrolled.count)"  $projectUnrolled -Separator "`n"
+
+    foreach ($directory in $projectUnrolled) {
+        $absDir,$found= Get-AbsolutePath -Path $directory -BaseDirectory $configDirectory
+
+        if ($found) {
+            Run-Command -command $command -directory $absDir
         } else {
             Write-Host "Directory '$directory' not found"
         }
@@ -82,7 +199,7 @@ function Invoke-Project {
 function Get-YamlConfig {
     param(
         [string]$configDirectory = ".",
-        [string]$configFile = "docker-project.yaml"
+        [string]$configFile = "docker-project.yml"
     )
 
     $configPath = Join-Path $configDirectory $configFile
@@ -92,22 +209,13 @@ function Get-YamlConfig {
         exit
     }
 
-    $config = Get-Content $configPath | Out-String | ConvertFrom-Yaml -UseMergingParser
+    $config = Get-Content $configPath | Out-String | ConvertFrom-Yaml -Ordered -UseMergingParser
     return $config
 }
 
-param (
-    [Parameter(Mandatory=$false)]
-    [string]$Project = "default",
-
-    [Parameter(Mandatory=$true)]
-    [string]$Command,
-
-    [Parameter(Mandatory=$false)]
-    [string]$ConfigDirectory = ".",
-
-    [Parameter(Mandatory=$false)]
-    [string]$ConfigFile = "docker-project.yaml"
-)
+if ($PSCmdlet.ParameterSetName -eq 'Help') {
+    Show-Help
+    exit
+}
 
 Invoke-Project -projectName $Project -command $Command -configDirectory $ConfigDirectory -configFile $configFile
